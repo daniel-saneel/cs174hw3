7b5becf9cc6b74c9a9e0bdd5073183cf0d74593a
Sachin <sachin.shah@sjsu.edu>
2020-10-25 14:23:09 -0700

    created and finished db config file and config file,

---
+++ src/configs/CreateDB.php
@@ -0,0 +1,77 @@
+<?php
+
+//import the config class to access the variables
+require_once('config.php');
+
+$dbconfig = new config();
+$servername = $dbconfig::SERVERNAME;
+$username = $dbconfig::USERNAME;
+$password = $dbconfig:: PASSWORD;
+
+
+//Create connection to local db 
+
+$connection = new mysqli($servername, $username, $password);
+
+//Check connection 
+if($connection-> connect_error) {
+    die("connection failed: " . $connection->connect_error);
+}
+//create db within the connection
+$sql = "CREATE DATABASE MovieReviewDB";
+
+if(mysqli_query($connection, $sql)){
+    echo "Database for moview reviews created successfully";
+}
+else {
+    echo "error creating database: " . $connection->error;
+}
+
+
+//Use this command to switch to using the MovieReviewDB 
+$useMovieDB = "use MovieReviewDB";
+
+if(mysqli_query($connection, $useMovieDB)){
+    echo "Now using the Movie Reviews DB \n";
+}
+else{
+    echo "error switching to Moview Review DB" . $connection->error;
+
+}
+
+
+//create genre table
+//primary key id 
+//fields: name
+$createGenreTable = "CREATE TABLE Genres(
+    genreId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name varchar(30) NOT NULL)";
+
+//create reviews table 
+//primary key is review ID 
+//fields: title, content, genreID, date
+//when inserting a review, the genre that it belongs to will be inserted into genreID
+$createReviewTable = "CREATE TABLE Reviews(
+    reviewId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    genreId INT(6) UNSIGNED,
+    title varchar(30) NOT NULL,
+    content TEXT NOT NULL,
+    insertDate DATE)";
+
+//run query for creating genres table 
+if(mysqli_query($connection, $createGenreTable)) {
+    echo "Genre table created successfully";
+}
+else {
+    echo "Error creating table: " . mysqli_error($connection);
+}
+
+//run query for creating review table 
+if(mysqli_query($connection, $createReviewTable)) {
+    echo "review table created successfully";
+}
+else {
+    echo "Error creating table: " . mysqli_error($connection);
+}
+
+
---
+++ src/configs/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+class config {
+
+    const USERNAME = "root";
+    const PASSWORD = "password";
+    const SERVERNAME = "127.0.0.1:3306";
+    const PORT = 3306;
+}
+
+?>
